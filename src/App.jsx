2d4082ed48ed14d62ac833e66829fcea22f9eb58
apupaul1 <apupaul.dev@gmail.com>
2026-03-03 20:03:10 +0600

    Setup React project with tailwind, daisyui

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { ToastContainer, toast } from 'react-toastify';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+
+
+
+
+      
+      <ToastContainer />
+    </>
+  )
+}
+
+export default App
